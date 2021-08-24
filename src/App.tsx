9067954f0dfaf592df616df54df8afbe9e954f65
Deepak Jacob <deepak.jacob@gmail.com>
2021-08-24 16:12:09 -0400

    feat: frame is loaded into the host page and visible always

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,35 @@
-import logo from "./logo.svg";
-import "./App.css";
-import * as AssetsService from "./services/assets-service";
 
-function App() {
+// import "./App.css";
+
+// IMPORTANT: use the asset service module to incluse assets like
+// images in your app. Find the usage below
+
+// import * as AssetsService from "./services/assets-service";
+// import logo from "./logo.svg";
+// AssetsService.getResourceURL(logo)
+import Frame, { FrameContextConsumer }from 'react-frame-component';
+
+interface XMainProps {
+  document?: Document;
+  window?: Window;
+  isExtension: boolean; 
+}
+const XMain = (props: XMainProps)=> (
+  <div>this is XMain component</div>
+)
+
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img
-          src={AssetsService.getResourceURL(logo)}
-          className="App-logo"
-          alt="logo"
-        />
-        <p>
-          Tuck Note, edit <code>src/App.tsx</code> and save to reload.
-        </p>
-      
-      </header>
-    </div>
-  );
+    <Frame head={[<link type="text/css" rel="stylesheet" href={chrome.runtime.getURL("/static/css/content.css")} ></link>]}> 
+      <FrameContextConsumer>
+      {
+        ({ document, window} :any) =>  <XMain document={document} window={window} isExtension={true}/> 
+                  
+      }
+      </FrameContextConsumer>
+    </Frame>
+  )
 }
 
 export default App;
