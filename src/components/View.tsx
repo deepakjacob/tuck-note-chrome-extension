453ea09eebe004ff16b91919c23c515fa997c966
Deepak Jacob <deepak.jacob@gmail.com>
2021-09-03 18:41:27 -0400

    feat: intitial markdown impl

--- src/components/View.tsx
+++ src/components/View.tsx
@@ -3,7 +3,7 @@ import Button from '@material-ui/core/Button';
 import { Provider, useDispatch } from 'react-redux';
 import { getSelectedNote } from '../store/actions/note';
 import configureStore from '../store/configureStore';
-import GloriousEditor from './GloriousEditor';
+import GloriousEditor from './editor/GloriousEditor';
 
 const store = configureStore();
 
